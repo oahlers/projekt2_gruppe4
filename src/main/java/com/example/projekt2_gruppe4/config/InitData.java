9ff558593b4ac74a7a226aeef2f7eae8df1d9ef3
oahlers <oliv2527@stud.kea.dk>
2025-03-31 11:23:51 +0200

    #3commit - various stuff added

--- src/main/java/com/example/projekt2_gruppe4/config/InitData.java
+++ src/main/java/com/example/projekt2_gruppe4/config/InitData.java
@@ -6,11 +6,11 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 
 @Component
-public class initData {
+public class InitData {
 
     private ArrayList<Product> productList = new ArrayList<>();
 
-    public initData() {
+    public InitData() {
         productList.add(new Product(1, "Øl", "Den kan drikkes", 18, ""));
         productList.add(new Product(2, "Cider", "Den kan drikkes", 18, ""));
         productList.add(new Product(3, "Faxe", "Den kan drikkes", 18, ""));
@@ -18,4 +18,6 @@ public class initData {
         productList.add(new Product(5, "Sprite", "Den kan drikkes", 18, ""));
     }
 
+    public ArrayList<Product> getProductList() { return productList;
+    }
 }
--- src/main/java/com/example/projekt2_gruppe4/controller/PageController.java
+++ src/main/java/com/example/projekt2_gruppe4/controller/PageController.java
@@ -1,13 +1,28 @@
 package com.example.projekt2_gruppe4.controller;
 
+import com.example.projekt2_gruppe4.config.InitData;
+import com.example.projekt2_gruppe4.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
 
 @Controller
 public class PageController {
 
+    @Autowired
+    InitData initData;
+
     @GetMapping("")
-    public String mainPage() {
+    public String mainPage(Model model) {
+        ArrayList<Product> productList = new ArrayList<>();
+
+        productList.addAll(initData.getProductList());
+
+        model.addAttribute("productList", productList);
+
         return "index";
     }
 }
