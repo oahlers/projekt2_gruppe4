4db6c5b3c49981b788a2304d98920fa0d60416cb
oahlers <oliv2527@stud.kea.dk>
2025-03-31 10:01:28 +0200

    Initial commit

---
+++ src/main/java/com/example/projekt2_gruppe4/Projekt2Gruppe4Application.java
@@ -0,0 +1,13 @@
+package com.example.projekt2_gruppe4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projekt2Gruppe4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projekt2Gruppe4Application.class, args);
+	}
+
+}
