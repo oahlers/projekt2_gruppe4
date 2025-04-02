071bdc25d929135666fcee8c443b38abbde940c9
oahlers <oliv2527@stud.kea.dk>
2025-04-02 08:26:41 +0200

    databaseauthenticator added. need autheticator database.sql

---
+++ src/main/java/com/example/projekt2_gruppe4/repository/DatabaseAuthenticator.java
@@ -0,0 +1,38 @@
+package com.example.projekt2_gruppe4.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.naming.AuthenticationException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Repository
+public class DatabaseAuthenticator {
+    private static DataSource dataSource;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        DatabaseAuthenticator.dataSource = dataSource;
+    }
+
+    public static int authenticate(String username, String password) throws AuthenticationException {
+        try (Connection connection = dataSource.getConnection()) {
+            String sql = "SELECT COUNT(*) FROM users WHERE username = ? AND password = ?";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1, username);
+            statement.setString(2, password);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    return resultSet.getInt("id");
+                }
+            }
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+        throw new AuthenticationException("Invald username and password");
+    }
+}
---
+++ src/main/java/com/example/projekt2_gruppe4/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.example.projekt2_gruppe4.repository;
+
+import com.example.projekt2_gruppe4.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public void CreateUser(User user) {
+        String sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+        jdbcTemplate.update(sql, user.getUsername(), user.getPassword());
+    }
+}
