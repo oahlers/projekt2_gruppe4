81447332f063213286dbf84e28c4e2757da71115
rasmusguldborg <rap00004@stud.kea.dk>
2025-04-03 11:52:52 +0200

    Ændringer i url til database

--- src/main/resources/databaseScript.sql
+++ src/main/resources/databaseScript.sql
@@ -1,8 +1,5 @@
-
 DROP DATABASE IF EXISTS ProductList;
-
 CREATE DATABASE ProductList;
-
 USE ProductList;
 
 CREATE TABLE products (
@@ -13,11 +10,19 @@ CREATE TABLE products (
                           img VARCHAR(255)
 );
 
-
--- Dummy data, Det er de samme objekter som bruges i InitData.
 INSERT INTO products (name, description, price, img) VALUES
                                                          ('Øl', 'Den kan drikkes', 18, 'øl.jpg'),
                                                          ('Cider', 'Den kan drikkes', 18, 'cider.jpeg'),
                                                          ('Faxe', 'Den kan drikkes', 18, 'faxe.jpg'),
                                                          ('Cola', 'Den kan drikkes', 18, 'cola.jpg'),
                                                          ('Sprite', 'Den kan drikkes', 18, 'sprite.webp');
+
+CREATE TABLE users (
+                       id INT AUTO_INCREMENT PRIMARY KEY,
+                       username VARCHAR(100) NOT NULL,
+                       password VARCHAR(100) NOT NULL
+);
+
+INSERT INTO users (username, password) VALUES
+                                           ('user1', 'password1'),
+                                           ('user2', 'password2');
