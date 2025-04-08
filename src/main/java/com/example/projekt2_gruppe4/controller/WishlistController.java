dd2f811f324f1f507700b83d15b302210243d040
rasmusguldborg <rap00004@stud.kea.dk>
2025-04-08 10:27:32 +0200

    Ændringer

--- src/main/java/com/example/projekt2_gruppe4/controller/WishlistController.java
+++ src/main/java/com/example/projekt2_gruppe4/controller/WishlistController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/wishlists")
@@ -31,6 +32,7 @@ public class WishlistController {
         if (loggedInUser == null) {
             return "redirect:/index";
         }
+        String shareToken = UUID.randomUUID().toString().substring(0, 8); // eks: "abc123ef"
 
         String insertQuery = "INSERT INTO wishlist (title, description, pincode, user_id) VALUES (?, ?, ?, ?)";
         jdbcTemplate.update(insertQuery, title, description, pincode, loggedInUser.getId());
@@ -75,4 +77,41 @@ public class WishlistController {
 
         return "viewWishlist"; // Return viewWishlist.html i resources/templates.
     }
+
+    @GetMapping("/shared/{token}")
+    public String showPincodeForm(@PathVariable("token") String token, Model model) {
+        model.addAttribute("token", token);
+        return "enterPincode"; // En side hvor man indtaster pinkoden
+    }
+
+    @PostMapping("/shared")
+    public String viewSharedWishlist(@RequestParam("token") String token,
+                                     @RequestParam("pincode") String pincode,
+                                     Model model) {
+        String sql = "SELECT * FROM wishlists WHERE share_token = ? AND pincode = ?";
+        List<Wishlist> wishlists = jdbcTemplate.query(sql, new Object[]{token, pincode}, new RowMapper<Wishlist>() {
+            @Override
+            public Wishlist mapRow(ResultSet rs, int rowNum) throws SQLException {
+                Wishlist wishlist = new Wishlist();
+                wishlist.setId(rs.getInt("id"));
+                wishlist.setUserId(rs.getInt("user_id"));
+                wishlist.setTitle(rs.getString("name"));
+                wishlist.setDescription(rs.getString("description"));
+                wishlist.setPincode(rs.getString("pincode"));
+                wishlist.setShareToken(rs.getString("share_token"));
+                return wishlist;
+            }
+        });
+
+        if (wishlists.isEmpty()) {
+            model.addAttribute("sharedError", "Forkert delingslink eller pinkode");
+            return "index"; // tilbage til forsiden med fejlbesked
+        }
+
+        Wishlist wishlist = wishlists.get(0);
+        model.addAttribute("wishlist", wishlist);
+        return "viewSharedWishlist"; // en side der viser ønskelisten
+    }
+
+
 }
